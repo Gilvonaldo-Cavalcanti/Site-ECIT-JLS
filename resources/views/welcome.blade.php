e8bfd26887da30515eedc28ee6ec905852b8153d
Gilvonaldo Cavalcanti <gilvonaldo@debian.gilvonaldo>
2022-04-26 13:54:08 -0300

    Adiçao do z-index no chat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,8 +18,7 @@
 <div class="row text-center mx-auto align-items-center">
   <div class="p-5 col">
     <h1>Nossa História</h1>
-    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum".</p>
-    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>    
+    <p>O Colégio Estadual de Monteiro, que hoje é a Escola Cidadã Integral Técnica José Leite de Souza, foi construída no ano de 1972 em um terreno doado pelo então Deputado Estadual Euvaldo da Silva Brito, que veio da iniciativa do Prefeito Jorge Rafael de Menezes e teve como diretor fundador o Fundador Dr. Antenor Campos. Atualmente o Colégio Estadual de Monteiro José leite de Souza, tem como responsável por sua gestão a Diretora Vera Paes.</p>
     <button type="button" class="btn btn-primary btn-lg">Leia mais</button>
   </div>
   <div class="col">
