319d291873b2bc0012aec73e2fe61a16211412cc
Gilvonaldo Cavalcanti <gilvonaldo@debian.gilvonaldo>
2022-06-12 12:32:54 -0300

    Atualização da página de Desenvolvedores

--- resources/views/equipe-desenvolvimento.blade.php
+++ resources/views/equipe-desenvolvimento.blade.php
@@ -6,4 +6,33 @@
 
 <h1>Página da Equipe de Desenvolvimento</h1>
 
+<div class="container-equipe">
+  <div class="row">
+    <div class="col">
+        <figure class="figure">
+            <img src="img/kelviny.jpeg" class="figure-img img-fluid rounded" alt="...">
+            <figcaption class="figure-caption text-end">Kelviny, 3º Ano C.</figcaption>
+        </figure>
+    </div>
+    <div class="col">
+        <figure class="figure">
+            <img src="img/lucas.jpeg" class="figure-img img-fluid rounded" alt="...">
+            <figcaption class="figure-caption text-end">Lucas, 3º Ano B</figcaption>
+        </figure>
+    </div>
+    <div class="col">
+        <figure class="figure">
+            <img src="..." class="figure-img img-fluid rounded" alt="...">
+            <figcaption class="figure-caption text-end">A caption for the above image.</figcaption>
+        </figure>
+    </div>
+    <div class="col">
+        <figure class="figure">
+            <img src="..." class="figure-img img-fluid rounded" alt="...">
+            <figcaption class="figure-caption text-end">A caption for the above image.</figcaption>
+        </figure>
+    </div>
+  </div>
+</div>
+
 @endsection
